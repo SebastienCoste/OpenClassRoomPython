f0fb39eab3d2b688b4eb36eb8535ee7fe96efc39
SebastienCoste <SebastienCoste@users.noreply.github.com>
2017-02-07 13:50:58 +0100

    the points in the maze

---
+++ maze/roboc/Point.py
@@ -0,0 +1,38 @@
+# -*-coding:Latin-1 -*
+'''
+Created on Feb 7, 2017
+@author: Static
+'''
+from roboc.RobocException import RobocException
+class Point:
+    
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        
+    def getNextPositions(self, direction, times):
+        result = []
+        copy = Point(self.height, self.width)
+        for _ in range(times):
+            copy = copy.getNext(direction)
+            result.append(copy)
+        
+        return result
+    
+    '''
+        Get the point to the next position
+        No validation, except if the direction is not understandable
+    '''
+    def getNext(self, direction):
+        nextPoint = Point(self.height, self.width)
+        if (direction == 'S'):
+            nextPoint.height +=1
+        elif (direction == 'N'):
+            nextPoint.height -=1
+        elif (direction == 'O'):
+            nextPoint.width -=1
+        elif (direction == 'E'):
+            nextPoint.width +=1
+        else:
+            raise RobocException("Lettre non reconnue (S N O E uniquement)")
+        return nextPoint
